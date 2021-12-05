d93a314ba5f3742fd3f66d96aef3c5428a7f834c
Jonas <jbenninkbolt@gmail.com>
2021-12-05 16:42:05 +0100

    day5_1 not working, answer too low

--- src/bin/day5.rs
+++ src/bin/day5.rs
@@ -26,11 +26,10 @@ struct Line {
 }
 
 impl Line {
-	fn new(line: &String) -> Line {
-		let both: Vec<&str> = line.split(" ").collect();
+	fn new(line: &String) -> Line { // with format of line = "x,y -> x,y"
 		Line {
-			bgn: Coord::new(both[0]),
-			end: Coord::new(both[2])
+			bgn: Coord::new(line.split(" ").nth(0).unwrap()),
+			end: Coord::new(line.split(" ").nth(2).unwrap())
 		}
 	}
 
@@ -39,14 +38,65 @@ impl Line {
 	}
 }
 
+const FIELD_SIZE: usize = 1000;
+struct Field {
+	f: Vec<Vec<i32>> // using an array will overflow stack
+}
+
+impl Field {
+	fn new() -> Field {
+		Field {
+			f: vec![vec![0; FIELD_SIZE]; FIELD_SIZE]
+		}
+	}
+}
+
 fn main() -> io::Result<()> {
 	let input:Vec<String> = read_lines("inputs/input_day5.txt")?.map(Result::unwrap).collect();
-	day5_1(&input);
+	println!("{}", day5_1(&input));
 	Ok(())
 }
 
-fn day5_1(input: &Vec<String>) {
+fn day5_1(input: &Vec<String>) -> i32 {
 	let mut all_lines: Vec<Line> = input.iter().map(|ln| Line::new(ln)).collect();
-	all_lines.retain(|line| line.bgn.x == line.end.x || line.bgn.y == line.end.y);
+	let mut field = Field::new();
+	// all_lines.retain(|line| line.bgn.x == line.end.x || line.bgn.y == line.end.y);
+	for line in all_lines {
+		draw_line(&line, &mut field);
+	}
+	count_higher_than_1(&field)
+}
 
+fn draw_line(line: &Line, field: &mut Field) {
+	if line.bgn.x == line.end.x {
+		if line.bgn.y < line.end.y {
+			for i in line.bgn.y..=line.end.y {
+				field.f[i as usize][line.bgn.x as usize] += 1;
+			}
+		} else {
+			for i in (line.bgn.y..=line.end.y).rev() {
+				field.f[i as usize][line.bgn.x as usize] += 1;
+			}
+		}
+	} else if line.bgn.y == line.end.y {
+		if line.bgn.x < line.end.x {
+			for i in line.bgn.x..=line.end.x {
+				field.f[line.bgn.y as usize][i as usize] += 1;
+			}
+		} else {
+			for i in (line.bgn.x..=line.end.x).rev() {
+				field.f[line.bgn.y as usize][i as usize] += 1;
+			}
+		}
+	}
+}
+
+fn count_higher_than_1(field: &Field) -> i32 {
+	field.f.iter()
+		.map(|row| row.iter()
+			.filter(|&&nr| nr >= 2)
+			.count() as i32)
+		.collect::<Vec<i32>>()
+		.iter()
+		.sum()
 }
