457e4a9122feb4975bc79faff10c605c46de1c0d
jbennink <jbennink@student.codam.nl>
2021-12-06 17:33:30 +0100

    day 6 done, surprisingly short

--- src/bin/day6.rs
+++ src/bin/day6.rs
@@ -1,14 +1,31 @@
 use std::io::{self};
 use aoc::*;
-use std::collections::VecDeque;
 
 fn main() -> io::Result<()> {
-	let lines:Vec<String> = read_lines("inputs/example.txt")?.map(Result::unwrap).collect();
-	let numbers: VecDeque<i32> = lines[0].split(",").map(|nr| nr.parse::<i32>().unwrap()).collect();
-	day6_1(numbers);
+	let lines:Vec<String> = read_lines("inputs/input_day6.txt")?.map(Result::unwrap).collect();
+	let numbers: Vec<usize> = lines[0].split(",").map(|nr| nr.parse::<usize>().unwrap()).collect();
+	println!("day 6_1: {}", day6(&numbers, 80));
+	println!("day 6_2: {}", day6(&numbers, 256));
 	Ok(())
 }
 
-fn day6_1(fish: VecDeque<i32>) {
+fn day6(fish: &Vec<usize>, days: u32) -> u64 {
+	let mut counters: [u64; 9] = get_start_vals(fish);
+	for _ in 1..=days {
+		let breeders = counters[0];
+		for i in 0..8 { counters[i] = counters[i + 1] }
+		counters[8] = breeders;
+		counters[6] += breeders;
+	}
+	counters.iter().sum()
+}
 
+fn get_start_vals(input: &Vec<usize>) -> [u64; 9] {
+	let mut ret: [u64; 9] = [0; 9];
+	for i in 0..9 {
+		ret[i] = input.iter()
+			.filter(|&&nr| nr == i)
+			.count() as u64
+	}
+	ret
 }
